28c91d7e37a25b667cbc55d666ccc1a723fe41ad
alexmonteil <alex.monteil@outlook.com>
2023-04-03 17:59:36 -0500

    end of part 1.c

--- lesson/src/App.js
+++ lesson/src/App.js
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 
+const Display = ({counter}) => <div>Watch me count: {counter}</div>;
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>;
 
 const App = () => {
   const [counter, setCounter] = useState(0);
-
-  setTimeout(() => setCounter(counter + 1), 1000);
+  const increment = () => setCounter(counter + 1);
+  const decrement = () => setCounter(counter - 1);
+  const reset = () => setCounter(0);
 
   return (
-    <div>Watch me count: {counter}</div>
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increment} text="+" />
+      <Button handleClick={decrement} text="-" />
+      <Button handleClick={reset} text="reset" />
+    </div>
   );
 }
 
