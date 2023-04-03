68b9b7f6a67aef8ed3c1d43554e0d9b6c634bfa7
alexmonteil <alex.monteil@outlook.com>
2023-04-02 20:53:16 -0500

    Init

---
+++ lesson/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  setTimeout(() => setCounter(counter + 1), 1000);
+
+  return (
+    <div>Watch me count: {counter}</div>
+  );
+}
+
+export default App;
