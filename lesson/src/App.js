26be0ec09319946516474ce322456a85901c84a9
alexmonteil <alex.monteil@outlook.com>
2023-04-03 21:06:24 -0500

    part 1.d  after conditional rendering section

--- lesson/src/App.js
+++ lesson/src/App.js
@@ -1,20 +1,47 @@
 import { useState } from 'react';
 
-const Display = ({counter}) => <div>Watch me count: {counter}</div>;
+// components
+const Display = ({left, right}) => <div>Left: {left}, Right: {right}</div>;
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>;
+const History = ({allClicks}) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>The app is used by pressing the buttons !</div>
+    );
+  }
+  
+  return (
+    <div>Button press history: {allClicks.join(' ')}</div>
+  );
+};
+
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const increment = () => setCounter(counter + 1);
-  const decrement = () => setCounter(counter - 1);
-  const reset = () => setCounter(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const handleLeft = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+  const handleRight = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
+
+  const reset = () => {
+    setLeft(0);
+    setRight(0);
+    setAll([]);
+  };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handleClick={increment} text="+" />
-      <Button handleClick={decrement} text="-" />
+      <Display left={left} right={right} />
+      <Button handleClick={handleLeft} text="Left" />
+      <Button handleClick={handleRight} text="Right" />
       <Button handleClick={reset} text="reset" />
+      <History allClicks={allClicks} />
     </div>
   );
 }
